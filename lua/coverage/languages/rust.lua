bdeeeb56701355e50d7c6a0323c7601f8920ba74
Adem Eren <adem.eren0598@gmail.com>
2024-05-13 14:48:21 +0200

    remove grcov json specifics

--- lua/coverage/languages/rust.lua
+++ lua/coverage/languages/rust.lua
@@ -1,86 +1,14 @@
 local M = {}
 
-local Path = require("plenary.path")
+local common = require("coverage.languages.common")
 local config = require("coverage.config")
-local signs = require("coverage.signs")
 local util = require("coverage.util")
 
 --- Returns a list of signs to be placed.
--- @param json_data from the generated report
-M.sign_list = function(json_data)
-    local sign_list = {}
-    for _, file in ipairs(json_data.source_files) do
-        local fname = Path:new(file.name):make_relative()
-        local buffer = vim.fn.bufnr(fname, false)
-        if buffer ~= -1 then
-            for linenr, hits in ipairs(file.coverage) do
-                if hits ~= nil and hits ~= vim.NIL then
-                    if hits > 0 then
-                        table.insert(sign_list, signs.new_covered(buffer, linenr))
-                    elseif hits == 0 then
-                        table.insert(sign_list, signs.new_uncovered(buffer, linenr))
-                    end
-                end
-            end
-        end
-    end
-    return sign_list
-end
+M.sign_list = common.sign_list
 
 --- Returns a summary report.
-M.summary = function(json_data)
-    local totals = {
-        statements = 0,
-        missing = 0,
-        excluded = nil,
-        branches = 0,
-        partial = 0,
-        coverage = 0,
-    }
-    local files = {}
-    for _, file in ipairs(json_data.source_files) do
-        local statements = 0
-        local missing = 0
-        local branches = 0
-        local partial = 0
-        local fname = Path:new(file.name):make_relative()
-        for _, hits in ipairs(file.coverage) do
-            totals.statements = totals.statements + 1
-            statements = statements + 1
-            if hits == 0 then
-                totals.missing = totals.missing + 1
-                missing = missing + 1
-            end
-        end
-        for i = 1, #file.branches - 1, 4 do
-            -- format: [line-number, block-number, branch-number, hits]
-            local hits = file.branches[i + 3]
-            totals.branches = totals.branches + 1
-            branches = branches + 1
-            if hits == 0 then
-                totals.partial = totals.partial + 1
-                partial = partial + 1
-            end
-        end
-        table.insert(files, {
-            filename = fname,
-            statements = statements,
-            missing = missing,
-            excluded = nil,
-            branches = branches,
-            partial = partial,
-            coverage = ((statements + branches - missing - partial) / (statements + branches)) * 100.0,
-        })
-    end
-    totals.coverage = (
-        (totals.statements + totals.branches - totals.missing - totals.partial)
-        / (totals.statements + totals.branches)
-    ) * 100.0
-    return {
-        files = files,
-        totals = totals,
-    }
-end
+M.summary = common.summary
 
 -- From http://lua-users.org/wiki/StringInterpolation.
 local interp = function(s, tab)
@@ -89,7 +17,7 @@ local interp = function(s, tab)
     end))
 end
 
---- Loads a coverage report.
+--- Loads a coverage report from stdout.
 -- @param callback called with the results of the coverage report
 M.load = function(callback)
     local rust_config = config.opts.lang.rust
